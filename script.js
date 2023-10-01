3f4070d098f83c76eeebcdb59cd1c08ae887fa61
NUR ALAM MONDAL <145200844+NUSH-9922@users.noreply.github.com>
2023-10-01 18:43:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// this function is help to show menu 
+function showMenu(){
+    document.getElementById("dropDown").style.right = "0%"
+}
+
+// this function is help to hide the menu 
+function hideMenu(){
+    document.getElementById("dropDown").style.right = "-500px"
+}
